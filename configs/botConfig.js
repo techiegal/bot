9ae6a09c291f4167d3d0f8576bd1b4f64b8a9ef7
MOUNIKA <my.social.on@gmail.com>
2024-02-27 10:54:32 +0530

    add

--- configs/botConfig.js
+++ configs/botConfig.js
@@ -2,11 +2,11 @@ const TelegramBot = require("node-telegram-bot-api");
 
 let botInstance;
 
-// Function to get the bot instance
 const getBotInstance = () => {
   if (!botInstance) {
     if (process.env.NODE_ENV === "production") {
       botInstance = new TelegramBot(process.env.BOT_TOKEN, {});
+      console.log("bot running on production");
     } else {
       botInstance = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
       console.log("bot running on development");
