d547a1105bb003501ca21cf231899bf75203e758
MOUNIKA <my.social.on@gmail.com>
2024-02-27 12:04:42 +0530

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = express();
 const url = process.env.SITE_URL;
 
